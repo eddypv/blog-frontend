64d5f03ffae1e3561c4bc2fff5ceed39de07cb7d
Eddy <eddypv0@gmail.com>
2021-07-15 17:30:13 -0400

    [UPDATE] Modify styles Blogs Component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,45 +1,31 @@
 import React from 'react'
 import propType  from 'prop-types'
 import { Link } from 'react-router-dom'
-
+import { Table } from 'react-bootstrap'
 function Blogs({ blogs }){
 
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-  /*
-
-  */
   return (
-    <div >
-      <div >
-        {blogs.map( item => {
-          /*let userBlog = {}
-          if(item.user){
-            userBlog = item.user
-          }
-          return <Blog
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            author={item.author}
-            likes={item.likes}
-            url={item.url}
-            userBlog={userBlog}
-            user={user}
-            handleSetLikes={handleSetLikes}
-            handleRemove={handleRemove}*/
-          return(
-            <div key={item.id} style={blogStyle}>
-              <Link to={`/blogs/${item.id}`}>{item.title} {item.author}</Link>
-            </div>
-          )
-        })}
-      </div>
+    <div className="container">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs.map( item => {
+            return(
+              <tr key={item.id} >
+                <td><Link to={`/blogs/${item.id}`}>{item.title}</Link></td>
+                <td>{item.author}</td>
+                <td>{item.user ? item.user.name : ''}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
     </div>
   )
 }
