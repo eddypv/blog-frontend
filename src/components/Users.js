589ba0944c8558ebe3f4d0fccfcb7ea57fd25932
Eddy <eddypv0@gmail.com>
2021-07-15 17:33:32 -0400

    [UPDATE] Modify styles Users Component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 
 const Users = ({ users }) => {
-
   return(
     <div>
       <h2>Users</h2>
       <div>
-        <table>
+        <Table striped bordered hover>
           <tbody>
             <tr>
-              <td></td>
+              <td>Name</td>
+              <td>Username</td>
               <td>Blogs created</td>
             </tr>
             {users.map( item => {
@@ -19,12 +20,13 @@ const Users = ({ users }) => {
                   <td>
                     <Link to={`/users/${item.id}`}>{item.name}</Link>
                   </td>
+                  <td>{item.username}</td>
                   <td>{item.blogs.length}</td>
                 </tr>
               )
             })}
           </tbody>
-        </table>
+        </Table>
       </div>
     </div>
   )
