59dc1962b7c07009f380a7d76a90855399c791a5
Eddy <eddypv0@gmail.com>
2021-07-16 15:50:43 -0400

    [UPDATE] Modify Notification Component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,13 +1,15 @@
-import './Notification.css'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Alert } from 'react-bootstrap'
+
 export default function Notification(){
   const notification = useSelector(state => state.notification)
   const { message, type } = notification
   if(message.trim() === ''){
     return null
   }
+  const variant = type ==='error' ? 'danger' :'success'
   return (
-    <p className={`Notification ${type}`}>{message}</p>
+    <Alert className={`Notification ${type}`} variant={variant}>{message}</Alert>
   )
 }
